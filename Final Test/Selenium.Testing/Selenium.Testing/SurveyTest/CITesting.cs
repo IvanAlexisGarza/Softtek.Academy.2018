9178db9d3024ecc8553873910abc7797c8e816e8
Ivan Alexis Garza Tapia <IvanAlexisGarza@gmail.com>
2018-04-10 13:16:41 -0700

    Selenium Test to Add Survey "Pass"

--- Final Test/Selenium.Testing/Selenium.Testing/SurveyTest/CITesting.cs
+++ Final Test/Selenium.Testing/Selenium.Testing/SurveyTest/CITesting.cs
@@ -7,14 +7,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Selenium.Testing.SurveyTest
 {
     class CITesting
     {
+
         [Test]
         public void OpenPage()
         {
+
             IWebDriver driver = new FirefoxDriver();
             driver.Url = "http://localhost:64226/";
 
@@ -23,13 +26,21 @@ namespace Selenium.Testing.SurveyTest
             driver.FindElement(By.XPath("/ html / body / div[2] / a")).Click();
 
 
+            IWebElement titleTextBoc = driver.FindElement(By.XPath("//*[@id='Title']"));
+            titleTextBoc.SendKeys("Selenium Test X");
 
-            driver.FindElement(By.XPath("//*[@id="IsActive"]")).Click();
+            IWebElement descriptonTextBox = driver.FindElement(By.XPath("//*[@id='Description']"));
+            descriptonTextBox.SendKeys("Selenium Test X");
+
+
+            driver.FindElement(By.XPath("//*[@id='IsActive']")).Click();
 
 
             SelectElement oSelect = new SelectElement(driver.FindElement(By.Id("Status")));
+            oSelect.SelectByText("Draft");
 
-            oSelect.selectByText("2010");
+            driver.FindElement(By.XPath("/html/body/div[2]/form/button")).Click();
+
 
             driver.Quit();
         }
